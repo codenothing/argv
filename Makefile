4144b0d4c8235b04a2fa9486e8c6a61ccdaf001e
Corey Hart <corey@codenothing.com>
2013-03-31 16:43:39 -0700

    -Upgarding munit package for running test suite across multiple versions of node

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 all: test
 
+clean:
+	@rm -rf build/results/
+
 lint:
 	@node build/lint.js
 
-test: lint
+test: clean lint
 	@node build/test.js
+
+test-full:
+	@./build/full.sh
--- build/test.js
+++ build/test.js
@@ -2,4 +2,7 @@ global.munit = require( 'munit' );
 global.argv = require( '../' );
 
 // Render all tests
-munit.render( __dirname + '/../test/', { junit: __dirname + '/results/' } );
+munit.render( __dirname + '/../test/', {
+	junit: __dirname + '/results/',
+	junitPrefix: process.version.replace( /\./g, '_' )
+});
